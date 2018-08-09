afb1e950383a772632cf6ea391e840b97ad70fd4
Geoff <geoff.register@gmail.com>
2018-08-09 15:18:05 -0400

    first error function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Add validation!');
+document.getElementById('parking-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    // clearError()
+
+    var name = document.getElementById('name').value.trim()
+
+    // check and make sure name is not empty
+
+    if (name === '') {
+        alert('ERROR')
+    }})
