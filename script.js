8cdb6fe33da9a01a6e98d5c1d87273915e32dc0a
Geoff <geoff.register@gmail.com>
2018-08-10 12:10:15 -0400

    Problem 1 Solved

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 document.getElementById('parking-form').addEventListener('submit', function (event) {
     event.preventDefault()
 
+    var inputElements = document.getElementsByTagName('input')
+
+    for (var i = 0; i < inputElements.length; i++) {
+        
+        if (inputElements[i].value.trim() === "") {
+            inputElements[i].parentElement.classList.add('input-invalid')
+        }
+        else {
+            inputElements[i].parentElement.classList.add('input-valid')
+        }
+
+}
+    })
+
+//         if (inputElements[i].value.trim() === "") {
+//             inputElements[i].createElement.parentElements.classList.add('input-invalid')
+//         }
+       
+//     }
+// })
+
+
+
+
+
+
+
+
     // clearError()
 
-    var name = document.getElementById('name').value.trim()
+//     var name = document.getElementById('name').value.trim()
+//     // check and make sure name is not empty
+//     if (name === '') {
+//        showInvalidError()
+//     }
+// })
 
-    // check and make sure name is not empty
+// function showInvalidError() {
+//     var errorDiv = document.createElement('div')
+//     errorDiv.classList.add('input-invalid')
 
-    if (name === '') {
-        alert('ERROR')
-    }})
+//     var field = document.getElementById('name-field')
+//     // field.parentElement.appendChild(errorDiv)
+//     field.classList.add('input-invalid')
+
+// }
+
+// for inputElement getElementsByTag('input')
