41e57084c5383d4d7c1713044227cceab5012b5e
Geoff <geoff.register@gmail.com>
2018-08-13 09:01:37 -0400

    final try

--- script.js
+++ script.js
@@ -3,28 +3,86 @@ document.getElementById('parking-form').addEventListener('submit', function (eve
 
     var inputElements = document.getElementsByTagName('input')
 
-    for (var i = 0; i < inputElements.length; i++) {
-        
-        if (inputElements[i].value.trim() === "") {
-            inputElements[i].parentElement.classList.add('input-invalid')
+    for (var idx = 0;idx < inputElements.length; idx++) {
+        if (inputElements[idx].value.trim() === '') {
+            inputElements[idx].parentElement.classList.add('input-invalid')
+            // errorMessage(inputElements[idx].parentElement)
         }
         else {
-            inputElements[i].parentElement.classList.add('input-valid')
+            inputElements[idx].parentElement.classList.add('input-valid')
         }
+    }
+})
 
-}
-    })
+// function errorMessage(inputElements[idx]) {
+//     var errorDiv = document.createElement('div')
+//     errorDiv.innerText = 'field is required'
+
+//     inputElements[idx].appendChild(errorDiv)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// inputElements[i].createElement('div')
+
+
+
+
+
+// function addErrorDiv() {
+//     var errorDiv = document.createElement('div')
+//     errorDiv.innerText = 'cannot be blank'
+
+// function showEmptyNameError () {
+//     var errorDiv = document.createElement('div')
+//     errorDiv.classList.add('error-msg')
+//     errorDiv.innerText = 'cannot be blank'
+
+
+
+
+
+
+
+
+
+
 
 //         if (inputElements[i].value.trim() === "") {
 //             inputElements[i].createElement.parentElements.classList.add('input-invalid')
 //         }
-       
+
 //     }
 // })
+  // for (var i = 0; i < inputElements.length; i++) {
 
 
+    // (var i of inputElements)
 
-
+ // inputElements[i].createElement.parentElement.add('div')
 
 
 
